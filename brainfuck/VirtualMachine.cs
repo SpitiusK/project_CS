04546eef8a8a47024d89b70c55cb4449e0c4706e
SpitiusK <exexd10@mail.ru>
2025-06-06 06:59:03 +0500

    realized VirtualMachine

--- brainfuck/VirtualMachine.cs
+++ brainfuck/VirtualMachine.cs
@@ -5,23 +5,34 @@ namespace func.brainfuck
 {
 	public class VirtualMachine : IVirtualMachine
 	{
-		public string Instructions { get; }
+		public string Instructions { get; set; }
 		public int InstructionPointer { get; set; }
 		public byte[] Memory { get; }
 		public int MemoryPointer { get; set; }
+		
+		public Dictionary<char, Action<IVirtualMachine>> Commands { get; }
 
 		public VirtualMachine(string program, int memorySize)
 		{
+			Instructions = program;
+			InstructionPointer = 0;
+			Memory = new byte[memorySize];
+			MemoryPointer = 0;
+			Commands = new Dictionary<char, Action<IVirtualMachine>>();
 		}
 
 		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
 		{
-			throw new NotImplementedException();
+			Commands.TryAdd(symbol, execute);
 		}
 
 		public void Run()
 		{
-			throw new NotImplementedException();
+			for (; InstructionPointer < Instructions.Length; InstructionPointer++)
+			{
+				var instruction = Instructions[InstructionPointer];
+				if (Commands.TryGetValue(instruction, out Action<IVirtualMachine> execute)) execute(this);
+			}
 		}
 	}
 }
