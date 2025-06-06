600301811c1cb82ce63de16ff1739ae227aadf7b
SpitiusK <exexd10@mail.ru>
2025-06-06 18:24:31 +0500

    Encapsulate all method in BrainfuckBasicCommands.cs

--- brainfuck/BrainfuckBasicCommands.cs
+++ brainfuck/BrainfuckBasicCommands.cs
@@ -2,61 +2,81 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace func.brainfuck
-{
-	public class BrainfuckBasicCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
-		{
-			vm.RegisterCommand('.', b => { write((char)b.Memory[vm.MemoryPointer]);});
-			vm.RegisterCommand('+', b => {
-				unchecked
-				{
-					b.Memory[vm.MemoryPointer]++;	
-				}
-			}); 
-			vm.RegisterCommand('-', b => {
-				unchecked
-				{
-					vm.Memory[b.MemoryPointer]--;
-				}
-			});
-			vm.RegisterCommand('>', b =>
-			{
-				if (b.MemoryPointer + 1 > b.Memory.Length - 1)
-				{
-					b.MemoryPointer = 0; 
-					return;
-				}
-				b.MemoryPointer++;
-			});
-			
-			vm.RegisterCommand('<', b =>
-			{
-				if (b.MemoryPointer - 1 < 0)
-				{
-					b.MemoryPointer = b.Memory.Length - 1;
-					return;
-				}
-				b.MemoryPointer--;
-			});
-			
-			vm.RegisterCommand(',', b =>
-			{
-				unchecked
-				{
-					var newReadValue = (byte)read();
-					b.Memory[vm.MemoryPointer] = newReadValue;
-				}
-			});
+namespace func.brainfuck;
 
-			foreach (var symbol in "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890")
-			{
-				vm.RegisterCommand(symbol, b =>
-				{
-					b.Memory[vm.MemoryPointer] = (byte)symbol;
-				});
-			}
-		}
+public class BrainfuckBasicCommands
+{
+	public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
+	{
+		vm.RegisterCommand('+', b => PerformUncheckedOperation(b, vm, value => ++value));
+
+		vm.RegisterCommand('-', b => PerformUncheckedOperation(b, vm, value => --value));
+
+		vm.RegisterCommand('>', b => MoveMemoryPointer(b, 1));
+
+		vm.RegisterCommand('<', b => MoveMemoryPointer(b, -1));
+
+		vm.RegisterCommand(',', b => PerformUncheckedOperation(b, vm, _ => (byte)read()));
+
+
+		RegisterAlphanumericCommands(vm);
 	}
+	
+	private static byte GetCurrentMemoryValue(IVirtualMachine vm, IVirtualMachine brainfuck)
+    {
+        return brainfuck.Memory[vm.MemoryPointer];
+    }
+        
+        
+    private static void SetCurrentMemoryValue(IVirtualMachine vm, IVirtualMachine brainfuck, byte value)
+    {
+        brainfuck.Memory[vm.MemoryPointer] = value;
+    }
+        
+        
+    private static void PerformUncheckedOperation(IVirtualMachine vm, IVirtualMachine brainfuck, Func<byte, byte> operation)
+    {
+        unchecked
+        {
+            byte currentValue = GetCurrentMemoryValue(vm, brainfuck);
+            byte newValue = operation(currentValue);
+            SetCurrentMemoryValue(vm, brainfuck, newValue);
+        }
+    }
+        
+        
+    private static void MoveMemoryPointer(IVirtualMachine brainfuck, int direction)
+    {
+        int newPointer = brainfuck.MemoryPointer + direction;
+        if (newPointer >= brainfuck.Memory.Length)
+        {
+            brainfuck.MemoryPointer = 0;
+        }
+        else if (newPointer < 0)
+        {
+            brainfuck.MemoryPointer = brainfuck.Memory.Length - 1;
+        }
+        else
+        {
+            brainfuck.MemoryPointer = newPointer;
+        }
+    }
+        
+        
+    private static void RegisterAlphanumericCommands(IVirtualMachine vm)
+    {
+     
+        var symbols = Enumerable.Range('A', 26) 
+            .Concat(Enumerable.Range('a', 26)) 
+            .Concat(Enumerable.Range('0', 10)) 
+            .Select(c => (char)c);
+
+            foreach (char symbol in symbols)
+            {
+                vm.RegisterCommand(symbol, b => SetCurrentMemoryValue(vm, b, (byte)symbol));
+            }
+    }
 }
+    
+
+
