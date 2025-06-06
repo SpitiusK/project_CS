3c90c1e7905996ae4de66dd0815bca7676d8c302
SpitiusK <exexd10@mail.ru>
2025-06-06 19:01:15 +0500

    Correct BrainfuckBasicCommands.cs, all tests are passing

--- brainfuck/BrainfuckBasicCommands.cs
+++ brainfuck/BrainfuckBasicCommands.cs
@@ -1,82 +1,44 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace func.brainfuck;
-
-public class BrainfuckBasicCommands
+namespace func.brainfuck
 {
-	public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
-	{
-		vm.RegisterCommand('+', b => PerformUncheckedOperation(b, vm, value => ++value));
-
-		vm.RegisterCommand('-', b => PerformUncheckedOperation(b, vm, value => --value));
-
-		vm.RegisterCommand('>', b => MoveMemoryPointer(b, 1));
-
-		vm.RegisterCommand('<', b => MoveMemoryPointer(b, -1));
-
-		vm.RegisterCommand(',', b => PerformUncheckedOperation(b, vm, _ => (byte)read()));
-
-
-		RegisterAlphanumericCommands(vm);
-	}
-	
-	private static byte GetCurrentMemoryValue(IVirtualMachine vm, IVirtualMachine brainfuck)
+    public static class BrainfuckBasicCommands
     {
-        return brainfuck.Memory[vm.MemoryPointer];
-    }
-        
-        
-    private static void SetCurrentMemoryValue(IVirtualMachine vm, IVirtualMachine brainfuck, byte value)
-    {
-        brainfuck.Memory[vm.MemoryPointer] = value;
-    }
-        
-        
-    private static void PerformUncheckedOperation(IVirtualMachine vm, IVirtualMachine brainfuck, Func<byte, byte> operation)
-    {
-        unchecked
+        private const string AlphanumericChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+
+        public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
         {
-            byte currentValue = GetCurrentMemoryValue(vm, brainfuck);
-            byte newValue = operation(currentValue);
-            SetCurrentMemoryValue(vm, brainfuck, newValue);
+            RegisterBasicCommands(vm, read, write);
+            RegisterAlphanumericCommands(vm);
         }
-    }
-        
-        
-    private static void MoveMemoryPointer(IVirtualMachine brainfuck, int direction)
-    {
-        int newPointer = brainfuck.MemoryPointer + direction;
-        if (newPointer >= brainfuck.Memory.Length)
-        {
-            brainfuck.MemoryPointer = 0;
-        }
-        else if (newPointer < 0)
-        {
-            brainfuck.MemoryPointer = brainfuck.Memory.Length - 1;
-        }
-        else
-        {
-            brainfuck.MemoryPointer = newPointer;
-        }
-    }
-        
-        
-    private static void RegisterAlphanumericCommands(IVirtualMachine vm)
-    {
-     
-        var symbols = Enumerable.Range('A', 26) 
-            .Concat(Enumerable.Range('a', 26)) 
-            .Concat(Enumerable.Range('0', 10)) 
-            .Select(c => (char)c);
 
-            foreach (char symbol in symbols)
+        private static void RegisterBasicCommands(IVirtualMachine vm, Func<int> read, Action<char> write)
+        {
+            vm.RegisterCommand('.', (IVirtualMachine v) => write((char)v.Memory[v.MemoryPointer]));
+            vm.RegisterCommand('+', (IVirtualMachine v) => AdjustMemory(v, 1));
+            vm.RegisterCommand('-', (IVirtualMachine v) => AdjustMemory(v, -1));
+            vm.RegisterCommand('>', (IVirtualMachine v) => MovePointer(v, 1));
+            vm.RegisterCommand('<', (IVirtualMachine v) => MovePointer(v, -1));
+            vm.RegisterCommand(',', (IVirtualMachine v) => v.Memory[v.MemoryPointer] = (byte)read());
+        }
+
+        private static void RegisterAlphanumericCommands(IVirtualMachine vm)
+        {
+            foreach (char c in AlphanumericChars)
             {
-                vm.RegisterCommand(symbol, b => SetCurrentMemoryValue(vm, b, (byte)symbol));
+                vm.RegisterCommand(c, (IVirtualMachine v) => v.Memory[v.MemoryPointer] = (byte)c);
             }
+        }
+
+        private static void AdjustMemory(IVirtualMachine vm, int amount)
+        {
+            vm.Memory[vm.MemoryPointer] = unchecked((byte)(vm.Memory[vm.MemoryPointer] + amount));
+        }
+
+        private static void MovePointer(IVirtualMachine vm, int direction)
+        {
+            int newPointer = vm.MemoryPointer + direction;
+            vm.MemoryPointer = (newPointer + vm.Memory.Length) % vm.Memory.Length;
+        }
     }
 }
-    
-
-
