6a1eb456ed73f66d4cf5c70ab1ac7341e63f1b57
SpitiusK <exexd10@mail.ru>
2025-06-10 02:09:04 +0500

    Превая версия BrainfuckLoop

--- brainfuck/BrainfuckLoopCommands.cs
+++ brainfuck/BrainfuckLoopCommands.cs
@@ -6,8 +6,48 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm)
 		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
+			var isFirst = true;
+			var jumpMap = new Dictionary<int, int>();
+			vm.RegisterCommand('[', b => 
+			{
+				if (isFirst)
+				{
+					jumpMap = CreateJumpMap(b.Instructions);
+					isFirst = false;
+				}
+
+				if (b.Memory[b.MemoryPointer] == 0)
+				{
+					b.InstructionPointer = jumpMap[b.InstructionPointer];
+				}
+			});
+			vm.RegisterCommand(']', b =>
+			{
+				if (b.Memory[b.MemoryPointer] == 0) return;
+				b.InstructionPointer = jumpMap[b.InstructionPointer];
+			});
+		}
+
+		public static Dictionary<int,int> CreateJumpMap(string instructions)
+		{
+			var resultJumpMap = new Dictionary<int, int>();
+			var bracketsStack = new Stack<(char, int)>();
+			for (var i = 0; i < instructions.Length; i++)
+			{
+				if (instructions[i] == '[')
+				{
+					bracketsStack.Push((instructions[i], i));
+					resultJumpMap[i] = 0; 
+				}
+				else if(instructions[i] == ']')
+				{
+					var currentBracket = bracketsStack.Pop();
+					resultJumpMap[currentBracket.Item2] = i;
+					resultJumpMap[i] = currentBracket.Item2;
+				}
+			}
+			
+			return resultJumpMap;
 		}
 	}
 }
