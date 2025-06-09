a69ff9c1f45ad2c92a5ac4bc247e3633faaadf76
SpitiusK <exexd10@mail.ru>
2025-06-10 02:10:54 +0500

    Отрефакторил BrainfuckLoopCommands

--- brainfuck/BrainfuckLoopCommands.cs
+++ brainfuck/BrainfuckLoopCommands.cs
@@ -1,53 +1,55 @@
+using System;
 using System.Collections.Generic;
 
 namespace func.brainfuck
 {
-	public class BrainfuckLoopCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm)
-		{
-			var isFirst = true;
-			var jumpMap = new Dictionary<int, int>();
-			vm.RegisterCommand('[', b => 
-			{
-				if (isFirst)
-				{
-					jumpMap = CreateJumpMap(b.Instructions);
-					isFirst = false;
-				}
+    public class BrainfuckLoopCommands
+    {
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            var jumpMap = CreateJumpMap(vm.Instructions);
+            vm.RegisterCommand('[', b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 0)
+                {
+                    b.InstructionPointer = jumpMap[b.InstructionPointer];
+                }
+            });
+            vm.RegisterCommand(']', b =>
+            {
+                if (b.Memory[b.MemoryPointer] != 0)
+                {
+                    b.InstructionPointer = jumpMap[b.InstructionPointer];
+                }
+            });
+        }
 
-				if (b.Memory[b.MemoryPointer] == 0)
-				{
-					b.InstructionPointer = jumpMap[b.InstructionPointer];
-				}
-			});
-			vm.RegisterCommand(']', b =>
-			{
-				if (b.Memory[b.MemoryPointer] == 0) return;
-				b.InstructionPointer = jumpMap[b.InstructionPointer];
-			});
-		}
-
-		public static Dictionary<int,int> CreateJumpMap(string instructions)
-		{
-			var resultJumpMap = new Dictionary<int, int>();
-			var bracketsStack = new Stack<(char, int)>();
-			for (var i = 0; i < instructions.Length; i++)
-			{
-				if (instructions[i] == '[')
-				{
-					bracketsStack.Push((instructions[i], i));
-					resultJumpMap[i] = 0; 
-				}
-				else if(instructions[i] == ']')
-				{
-					var currentBracket = bracketsStack.Pop();
-					resultJumpMap[currentBracket.Item2] = i;
-					resultJumpMap[i] = currentBracket.Item2;
-				}
-			}
-			
-			return resultJumpMap;
-		}
-	}
+        public static Dictionary<int, int> CreateJumpMap(string instructions)
+        {
+            var resultJumpMap = new Dictionary<int, int>();
+            var bracketsStack = new Stack<int>();
+            for (var i = 0; i < instructions.Length; i++)
+            {
+                if (instructions[i] == '[')
+                {
+                    bracketsStack.Push(i);
+                }
+                else if (instructions[i] == ']')
+                {
+                    if (bracketsStack.Count == 0)
+                    {
+                        throw new InvalidOperationException($"Unmatched ']' at position {i}");
+                    }
+                    var openPos = bracketsStack.Pop();
+                    resultJumpMap[openPos] = i;
+                    resultJumpMap[i] = openPos;
+                }
+            }
+            if (bracketsStack.Count > 0)
+            {
+                throw new InvalidOperationException($"Unmatched '[' at position {bracketsStack.Peek()}");
+            }
+            return resultJumpMap;
+        }
+    }
 }
