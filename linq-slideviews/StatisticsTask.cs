9af5a025fcba9498150e02813a918dddd094e136
SpitiusK <exexd10@mail.ru>
2025-06-12 11:00:26 +0500

    Реализовал StatististicsTask

--- linq-slideviews/StatisticsTask.cs
+++ linq-slideviews/StatisticsTask.cs
@@ -8,6 +8,16 @@ public class StatisticsTask
 {
 	public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
 	{
-		throw new NotImplementedException();
+		var times = visits
+			.GroupBy(visit => visit.UserId)
+			.SelectMany(group => group
+				.OrderBy(visit => visit.DateTime)
+				.Bigrams()
+				.Select(bigram => (visitTime: bigram.Second.DateTime - bigram.First.DateTime,
+					slideType: bigram.First.SlideType))
+				.Where(timeInfo => timeInfo.slideType == slideType
+				                   && timeInfo.visitTime.TotalMinutes is >= 1 and <= 120)
+				.Select(time => time.visitTime.TotalMinutes)).ToList();
+		return times.Any() ? times.Median() : 0;
 	}
 }
