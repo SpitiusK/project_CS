f4c032c871d23bc661db59d0f7ad8cc46e1eaba6
SpitiusK <exexd10@mail.ru>
2025-06-17 11:17:13 +0500

    Оптимизировал RivalsTask, улучшил читаемость, добавил минимальную документацию

--- Rivals/RivalsTask.cs
+++ Rivals/RivalsTask.cs
@@ -12,27 +12,61 @@ public class RivalsTask
         public int Distance; // Расстояние от стартовой позиции игрока
         public int Time;     // Время достижения клетки
 
-        public CellInfo()
+        public static CellInfo Unreachable => new()
         {
-            Owner = -1;
-            Distance = -1;
-            Time = int.MaxValue;
-        }
+            Owner = -1,
+            Distance = -1,
+            Time = int.MaxValue
+        };
     }
 
+    private static readonly Point[] MovementDirections =
+    {
+        new(0, -1), // Вверх
+        new(0, 1),  // Вниз
+        new(-1, 0), // Влево
+        new(1, 0)   // Вправо
+    };
+
+    /// <summary>
+    /// Assigns owners to map cells based on the fastest player to reach each cell.
+    /// </summary>
     public static IEnumerable<OwnedLocation> AssignOwners(Map map)
     {
+        if (map.Players.Length == 0)
+            return Enumerable.Empty<OwnedLocation>();
+
         int mapWidth = map.Maze.GetLength(0);
         int mapHeight = map.Maze.GetLength(1);
         int totalPlayers = map.Players.Length;
 
-        // Инициализация массива информации о клетках
-        var cellInfos = new CellInfo[mapWidth, mapHeight];
+        // Инициализация клеток и стартовых позиций
+        var cellOwners = InitializeCellOwners(map, mapWidth, mapHeight, totalPlayers);
+
+        // Создание множества сундуков
+        var chests = CreateChestsSet(map);
+
+        // Обработка территории каждого игрока
+        for (int playerIndex = 0; playerIndex < totalPlayers; playerIndex++)
+        {
+            ExplorePlayerTerritory(map, playerIndex, totalPlayers, cellOwners, chests);
+        }
+
+        // Сбор результатов
+        return CollectResults(cellOwners, mapWidth, mapHeight);
+    }
+
+    /// <summary>
+    /// Initializes the cell ownership array and sets starting positions for players.
+    /// </summary>
+    private static CellInfo[,] InitializeCellOwners(Map map, int mapWidth, int mapHeight, int totalPlayers)
+    {
+        var cellOwners = new CellInfo[mapWidth, mapHeight];
+
+        // Инициализация всех клеток как недостижимых
         for (int x = 0; x < mapWidth; x++)
             for (int y = 0; y < mapHeight; y++)
-                cellInfos[x, y] = new CellInfo();
-
-        var chests = new HashSet<Point>(map.Chests);
+                cellOwners[x, y] = CellInfo.Unreachable;
 
         // Установка стартовых позиций игроков
         for (int playerIndex = 0; playerIndex < totalPlayers; playerIndex++)
@@ -40,7 +74,7 @@ public class RivalsTask
             var startPosition = map.Players[playerIndex];
             if (map.InBounds(startPosition) && map.Maze[startPosition.X, startPosition.Y] == MapCell.Empty)
             {
-                cellInfos[startPosition.X, startPosition.Y] = new CellInfo
+                cellOwners[startPosition.X, startPosition.Y] = new CellInfo
                 {
                     Owner = playerIndex,
                     Distance = 0,
@@ -49,30 +83,22 @@ public class RivalsTask
             }
         }
 
-        var directions = new Point[]
-        {
-            new Point(0, -1), // Вверх
-            new Point(0, 1),  // Вниз
-            new Point(-1, 0), // Влево
-            new Point(1, 0)   // Вправо
-        };
-
-        // Запуск BFS для каждого игрока
-        for (int playerIndex = 0; playerIndex < totalPlayers; playerIndex++)
-        {
-            ExplorePlayerTerritory(map, playerIndex, totalPlayers, cellInfos, chests, directions);
-        }
-
-        // Сбор результатов
-        return Enumerable.Range(0, mapHeight)
-            .SelectMany(y => Enumerable.Range(0, mapWidth)
-                .Where(x => cellInfos[x, y].Time < int.MaxValue)
-                .Select(x => new OwnedLocation(cellInfos[x, y].Owner, new Point(x, y), cellInfos[x, y].Distance)))
-            .ToList();
+        return cellOwners;
     }
 
+    /// <summary>
+    /// Creates a set of chest positions for efficient lookup.
+    /// </summary>
+    private static HashSet<Point> CreateChestsSet(Map map)
+    {
+        return new HashSet<Point>(map.Chests);
+    }
+
+    /// <summary>
+    /// Explores the territory of a single player using BFS.
+    /// </summary>
     private static void ExplorePlayerTerritory(Map map, int playerIndex, int totalPlayers,
-        CellInfo[,] cellInfos, HashSet<Point> chests, Point[] directions)
+        CellInfo[,] cellOwners, HashSet<Point> chests)
     {
         var startPosition = map.Players[playerIndex];
         if (!map.InBounds(startPosition) || map.Maze[startPosition.X, startPosition.Y] == MapCell.Wall)
@@ -88,39 +114,71 @@ public class RivalsTask
             var (position, distance) = queue.Dequeue();
             int time = distance == 0 ? 0 : playerIndex + (distance - 1) * totalPlayers;
 
-            // Обновляем клетку только если новое время меньше текущего
-            if (time < cellInfos[position.X, position.Y].Time)
-            {
-                cellInfos[position.X, position.Y] = new CellInfo
-                {
-                    Owner = playerIndex,
-                    Distance = distance,
-                    Time = time
-                };
-            }
+            // Обновление владельца клетки
+            UpdateCellOwner(cellOwners, position, playerIndex, distance, time);
 
-            // Если клетка не сундук, проверяем соседние клетки
+            // Проверка и добавление соседних клеток
             if (!chests.Contains(position))
             {
-                foreach (var direction in directions)
-                {
-                    var nextPosition = position + direction;
-                    int nextTime = playerIndex + distance * totalPlayers;
-                    if (IsValidMove(map, nextPosition, visited, cellInfos, nextTime))
-                    {
-                        queue.Enqueue((nextPosition, distance + 1));
-                        visited.Add(nextPosition);
-                    }
-                }
+                TryAddNeighborsToQueue(map, position, distance, playerIndex, totalPlayers, cellOwners, visited, queue);
             }
         }
     }
 
-    private static bool IsValidMove(Map map, Point position, HashSet<Point> visited, CellInfo[,] cellInfos, int nextTime)
+    /// <summary>
+    /// Updates the cell owner if the new time is less than the current time.
+    /// </summary>
+    private static void UpdateCellOwner(CellInfo[,] cellOwners, Point position, int playerIndex, int distance, int time)
     {
-        return map.InBounds(position) &&
-               map.Maze[position.X, position.Y] == MapCell.Empty &&
-               !visited.Contains(position) &&
-               cellInfos[position.X, position.Y].Time > nextTime;
+        if (time < cellOwners[position.X, position.Y].Time)
+        {
+            cellOwners[position.X, position.Y] = new CellInfo
+            {
+                Owner = playerIndex,
+                Distance = distance,
+                Time = time
+            };
+        }
+    }
+
+    /// <summary>
+    /// Tries to add neighboring cells to the BFS queue if they are valid.
+    /// </summary>
+    private static void TryAddNeighborsToQueue(Map map, Point position, int distance, int playerIndex, int totalPlayers,
+        CellInfo[,] cellOwners, HashSet<Point> visited, Queue<(Point Position, int Distance)> queue)
+    {
+        int nextTime = playerIndex + distance * totalPlayers;
+        foreach (var direction in MovementDirections)
+        {
+            var nextPosition = position + direction;
+            if (IsValidMove(map, nextPosition, visited, cellOwners, nextTime))
+            {
+                queue.Enqueue((nextPosition, distance + 1));
+                visited.Add(nextPosition);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Checks if a move to the given position is valid.
+    /// </summary>
+    private static bool IsValidMove(Map map, Point position, HashSet<Point> visited,
+        CellInfo[,] cellOwners, int nextTime)
+    {
+        return map.InBounds(position) &&                    // Клетка в пределах карты
+               map.Maze[position.X, position.Y] == MapCell.Empty && // Клетка не стена
+               !visited.Contains(position) &&              // Клетка не посещена
+               cellOwners[position.X, position.Y].Time > nextTime; // Новое время меньше текущего
+    }
+
+    /// <summary>
+    /// Collects the final results as a list of owned locations.
+    /// </summary>
+    private static IEnumerable<OwnedLocation> CollectResults(CellInfo[,] cellOwners, int mapWidth, int mapHeight)
+    {
+        return Enumerable.Range(0, mapHeight)
+            .SelectMany(y => Enumerable.Range(0, mapWidth)
+                .Where(x => cellOwners[x, y].Time < int.MaxValue)
+                .Select(x => new OwnedLocation(cellOwners[x, y].Owner, new Point(x, y), cellOwners[x, y].Distance)));
     }
 }
